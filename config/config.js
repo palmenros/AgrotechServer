1ead7729115bdae7f1904b956d564876f282b367
Pedro Palacios Almendros <palmenros@gmail.com>
2022-12-19 19:20:12 +0100

    Bumped versions

--- config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    //OAUTH2 configuration
-    user: 'noreplyagrotech@gmail.com',
-    refreshToken: '1/Pul7lFdn3o0V0-Q3OBKc0qO0XNqfvbPKDxDTrPVjN7iDpCVhyFiUFCTNrTl3M_Xr',
-    clientId: '737431153706-8sh8l829lvhmpiqr9fe4rrsnr03odld6.apps.googleusercontent.com',
-    clientSecret: 'dOm88AcwubMQjYv6WED3DiQd'
-};
